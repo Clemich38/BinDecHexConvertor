43fde6e56e85f5cd285bfcd4ae5f14be664362af
Clemich38 <clement.letreguilly@gmail.com>
2017-02-23 15:21:33 +0900

    name corrected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Realtime Convertor';
+  title = 'Realtime Converter';
 
   public binStr: string;
   public hexStr: string;
